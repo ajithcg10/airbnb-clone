4634a5c5078aef0158c2586359e49438b9bf515f
ajith.steyp <ajith.steyp@gmail.com>
2023-05-29 15:08:05 +0530

    firbase-sign up functionality complete

--- src/LoginPage.jsx
+++ src/LoginPage.jsx
@@ -1,14 +1,30 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { styled } from "styled-components";
 import { auth, provider } from "./config";
 import { signInWithPopup } from "firebase/auth";
 
 export default function LoginPage() {
+  const [value, setValue] = useState("");
+  const handleclick = () => {
+    signInWithPopup(auth, provider).then((data) => {
+      setValue(data.user.email);
+      localStorage.setItem("email", data.user.email);
+    });
+  };
+  useEffect(() => {
+    setValue(localStorage.getItem("email"));
+  });
   return (
     <div>
       <MainContainer>
         <TopContainer>
-          <Sign>Sign up</Sign>
+          <Sign
+            onClick={() => {
+              handleclick();
+            }}
+          >
+            Sign up
+          </Sign>
           <Login>Login</Login>
         </TopContainer>
         <BottomContainer>
